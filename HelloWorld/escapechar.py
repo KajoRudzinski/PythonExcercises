6632d0953567bfbd7a4bdad41d9548798c3367d9
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-12 14:42:49 +0200

    Playing with \ as escape char to manipulate tabs, quotes & line splitting

---
+++ HelloWorld/escapechar.py
@@ -0,0 +1,28 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+# using \ with quotes
+
+print('The pet shop owner said "No, no, \'e\'s uh,... he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,... he's resting\".")
+
+# tripple quotes
+print("""The pet shop owner saig "No, no, 'e's uh,... he's resting".""")
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+
+print(anotherSplitString)
+
+notSplitString = """This string has not been \
+split over \
+several \
+lines"""
+
+print(notSplitString)
