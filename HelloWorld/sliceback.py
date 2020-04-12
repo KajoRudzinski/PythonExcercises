00dc84251c56db5115b747d1b7910939acd3d563
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-12 22:54:07 +0200

    String slices

---
+++ HelloWorld/sliceback.py
@@ -0,0 +1,15 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25:0:-1]    # zyxwvutsrqponmlkjihgfedcb
+print(backwards)
+
+reverseSequence = letters[::-1]     # prints reverse
+print(reverseSequence)
+
+# common idioms in python
+
+# end of the sequence
+print(letters[-1:])
+
+# start of the sequence
+print(letters[:1])
--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -7,3 +7,25 @@ print()
 
 print(parrot[-1])       # from behind, starting from -1: "e"
 print(parrot[13-14])    # the same (-1): "e"
+
+# string slicing
+
+# [starts at including:ends not including]
+print(parrot[0:6])      # Norweg
+print(parrot[3:5])      # we
+print(parrot[0:9])      # Norwegian
+print(parrot[:9])       # Norwegian
+
+print(parrot[10:14])    # Blue
+print(parrot[10:])      # Blue
+
+print(parrot[:6] + parrot[6:])  # Norwegian Blue
+
+# slicing with step
+print(parrot[0:6:2])    # Nre > every 2nd char
+print(parrot[0:6:3])    # Nw  > every 3rd char
+
+number = "9,223:902;222 523:333,980"
+separators = number[1::4]
+print(separators)   # could be useful working with weird data
+
