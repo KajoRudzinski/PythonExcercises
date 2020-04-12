85aad7def316affe5ffe6b6e5b1b883f64c0b147
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-12 17:38:14 +0200

    Variables int & str

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -6,8 +6,19 @@ print('test including "quotes" ')
 print('string'+' concat')
 
 greeting = "Hello"
-name = input("Please enter your name ")
-
-# input function stores provided value in a variable
+name = "George"
 
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+# print variable type
+print(type(greeting))
+print(type(age))
+
+age = "2 years" #  unlike JAVA and C we can change the variable type in Python
+print(type(age))
+
+# automatic type convertion won't work, like:
+# print(name + 2)   >>> produces error
