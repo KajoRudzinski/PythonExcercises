f0a4e37438d33217cddd851a4f86a460af99cf94
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-12 18:41:43 +0200

    Positive and negative string indexing

---
+++ HelloWorld/strings2.py
@@ -0,0 +1,9 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[3])    # print individual char, 0 based: "w"
+
+print()
+
+print(parrot[-1])       # from behind, starting from -1: "e"
+print(parrot[13-14])    # the same (-1): "e"
