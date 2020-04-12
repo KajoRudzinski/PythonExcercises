c2387e5bd16cd7d5702e1f47eb68144d1e8a3396
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-12 14:27:51 +0200

    Expanded helloworld, created & played with strings

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,1 +1,6 @@
-print("Hello there, mate :)")
+print('Hello there, mate :)')
+print(1+2)
+print(7*6)
+print()
+print('The end', 'or is it?', 'whatever', 3)
+
---
+++ HelloWorld/strings.py
@@ -0,0 +1,13 @@
+# testing various examples with quotes
+
+print('today is a good day to die')
+print("your mother's watch")
+print('test including "quotes" ')
+print('string'+' concat')
+
+greeting = "Hello"
+name = input("Please enter your name ")
+
+# input function stores provided value in a variable
+
+print(greeting + ' ' + name)
