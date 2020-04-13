3dabe412edc72e75a4da837c2428b22a3d291e89
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-13 23:39:24 +0200

    Else if

--- FlowControl/blocks.py
+++ FlowControl/blocks.py
@@ -10,15 +10,22 @@ for i in range(1, 13):
 
 # Basically, indentation tells python if this is still that block
 
-
 # If statements
 name = input("Please enter your name: ")
 age = int(input("How old are you, {}? ".format(name)))
 print(age)
 
-if age >= 18:
+# if age >= 18:
+#     print("You're old enough to vote")
+#     print("Please put an X in the box")
+# else:
+#     print("Please come back in {} years".format(18 - age))
+
+# Else if
+if age < 18:
+    print("Please come back in {} years".format(18 - age))
+elif age == 900:
+    print("Go away, Yoda!")
+else:
     print("You're old enough to vote")
     print("Please put an X in the box")
-else:
-    print("Please come back in {} years".format(18 - age))
-
