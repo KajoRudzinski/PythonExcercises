8602542b4ce590d879f1ed34678051a52ed4a384
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-14 23:20:22 +0200

    bool operators

---
+++ FlowControl/truefalse.py
@@ -0,0 +1,15 @@
+# day = "Monday"
+# temperature = 30
+# raining = False
+#
+# if (day == "Saturday" and temperature > 27) or not raining:
+#     print("Swimming")
+# else:
+#     print("Learning")
+
+# below, 0 is evaluated to false
+# so is empty string and None
+if 0:
+    print("true")
+else:
+    print("false")
