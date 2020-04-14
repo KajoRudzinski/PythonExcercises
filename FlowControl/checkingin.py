97c54b099b9cd0338690c56da75300cf93e827f6
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-14 23:24:47 +0200

    .casefold = lowercase

--- FlowControl/checkingin.py
+++ FlowControl/checkingin.py
@@ -2,7 +2,7 @@ parrot = "Norwegian Blue"
 
 letter = input("Enter a character: ")
 
-if letter in parrot:
+if letter.casefold() in parrot: # casefold = lowercase
     print("{} is in {}".format(letter, parrot))
 else:
     print("I don't need that letter")
