23a4e3f3d8a7f2945eb7e35a469db3d117b19a79
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-15 00:06:12 +0200

    for loop using range

---
+++ FlowControl/forloop.py
@@ -0,0 +1,6 @@
+parrot = "Norwegian Blue"
+
+# interestingly, python knows to check the string
+# because string is an iterable item
+for character in parrot:
+    print(character)
---
+++ FlowControl/ranges.py
@@ -0,0 +1,11 @@
+# again, up to but not including
+for i in range(1, 20):
+    print("i is now{} ".format(i))
+
+# default start is 0
+for i in range(20):
+    print("i is now{} ".format(i))
+
+# 3rd value is step, here gives every second value
+for i in range(0, 20, 2):
+    print("i is now{} ".format(i))
