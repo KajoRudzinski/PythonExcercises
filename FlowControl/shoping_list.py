79090e420cfa3e28ef8d8f28f9a240d2bf430e72
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-17 23:06:50 +0200

    while loop

--- FlowControl/shoping_list.py
+++ FlowControl/shoping_list.py
@@ -3,10 +3,14 @@ shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 item_to_find = "spam"
 found_at = None
 
-for index in range(len(shopping_list)):
-    if shopping_list[index] == item_to_find:
-        found_at = index
-        break
+# for index in range(len(shopping_list)):
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
+
+# simpler
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
 
 
 print("Item found at position {}".format((found_at)))
