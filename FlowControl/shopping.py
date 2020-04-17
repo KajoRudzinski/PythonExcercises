79c0fe9fb46cd1f4ede66beffc4357483f913225
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-17 22:52:51 +0200

    loops - break

--- FlowControl/shopping.py
+++ FlowControl/shopping.py
@@ -1,13 +1,8 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
-# for item in shopping_list:
-#     if item != "spam":
-#         print("Buy" + item)
-
-# or (better) to exlude item from the loop
 
 for item in shopping_list:
     if item == "spam":
-        continue    # 'skips' the remaining code
+        break
 
     print("Buy" + item)
