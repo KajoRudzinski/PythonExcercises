9516e0a62425102ce6640ed4dc6556e2c9941d48
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-19 22:59:41 +0200

    comparing lists, and lists containing other lists

--- ListsRangesTuplets/lists.py
+++ ListsRangesTuplets/lists.py
@@ -1,21 +1,41 @@
-parrot_list = [
-    "non pinin",
-    "no more",
-    "a stiff",
-    "bereft of Life"
-]
+# 2 ways of creating empty lists
+list_1 = []
+list_2 = list()
 
-parrot_list.append("A Norwegian Blue")
+print("list 1: {}".format(list_1))
+print("list 2: {}".format(list_2))
 
-for state in parrot_list:
-    print("This parrot is " + state)
+if list_1 == list_2:
+    print("List are equal")
+
+# Passing members of a string
+# because string is iterable
+# to new list:
+
+print(list("The new list"))
 
 even = [2, 4, 6, 8]
-odd = [1, 3, 5, 7, 9]
+another_even = even
 
-numbers = even + odd
-numbers.sort()
+print(another_even is even)
+# returns true > for python these are the same
+
+# BUT...:
+another_even_x = list(even)
+print(another_even_x is even)
+# returns false
+
+# BUT...:
+print(another_even_x == even)
+# returns true, because it compares the content
+# not object identity
+
+odd = [1, 3, 5, 7]
+
+numbers = [even, odd]  # creates list with 2 lists in them
 print(numbers)
 
-# same as above can be achieved by
-print(sorted(numbers))
+for number_set in numbers:
+    print(number_set)
+    for value in number_set:
+        print(value)
