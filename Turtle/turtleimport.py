c4233778862cf6db90efb9322ddb748b78ddd0bf
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-04-24 22:49:15 +0200

    import examle

---
+++ Turtle/turtleimport.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(200)
+
+time.sleep(4)
+
+# you could also import only part of the module like
+# from turtle import forward, right
